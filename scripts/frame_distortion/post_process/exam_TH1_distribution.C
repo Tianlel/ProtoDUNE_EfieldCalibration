30ba91d8564444b29829e7d46353d7e3234def6e
Tianle <tianlel@FNAL.gov>
2020-08-05 15:59:58 -0500

    code to display a 1D histogram

---
+++ scripts/frame_distortion/post_process/exam_TH1_distribution.C
@@ -0,0 +1,20 @@
+#include <fstream>
+
+/* check distribution for particular bins */
+
+void exam_TH1_distribution()
+{
+    string PATH = "/dune/app/users/tianlel/protoDUNE/E_field/ProtoDUNE_EfieldCalibration/scripts/frame_distortion/";
+    TFile *f = TFile::Open("/dune/app/users/tianlel/protoDUNE/E_field/ProtoDUNE_EfieldCalibration/output_ROOTtree/reco/frame_distortion/updateTH2.root", "READ");
+
+    TStyle *st = new TStyle("Modern","my style");
+    st->SetPadGridX(1);
+    st->SetPadGridY(1);
+    st->cd();
+    gStyle->SetOptStat(0);
+
+    TH1F *h; 
+    f->GetObject("deltaT_pos_32_1", h);
+    h->Draw();
+    
+}
