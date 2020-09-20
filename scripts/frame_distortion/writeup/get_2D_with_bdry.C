1780905de2f78793a6a0b6d0bed1dc0cbbdb0085
Tianle <tianlel@FNAL.gov>
2020-09-20 18:23:31 -0500

    update plots

--- scripts/frame_distortion/writeup/get_2D_with_bdry.C
+++ scripts/frame_distortion/writeup/get_2D_with_bdry.C
@@ -1,7 +1,6 @@
 
 void get_2D_with_bdry()
 {
-    gROOT->SetBatch(kTRUE);
     string INPUT_FILE = "ALLEVENTS_without_thermal_unordered_dT.root";
     string PATH = "/dune/app/users/tianlel/protoDUNE/E_field/ProtoDUNE_EfieldCalibration/output_ROOTtree/reco/frame_distortion/";
     string OUTPUT_PATH = "/dune/app/users/tianlel/protoDUNE/E_field/ProtoDUNE_EfieldCalibration/plots/";
@@ -20,17 +19,19 @@ void get_2D_with_bdry()
     f->GetObject("deltaT_YZ_h2_err0_neg", deltaT_YZ_h2_neg);
 
     TCanvas *c1 = new TCanvas("c1", "c1", 1200, 1000);
-    deltaT_YZ_h2->SetMarkerSize(0.8);
+    deltaT_YZ_h2->SetMarkerSize(0.7);
+    deltaT_YZ_h2->SetTitle("beam left (x>0);z (cm);y (cm); dT (ticks)");
     deltaT_YZ_h2->Draw("COLZ TEXT");
-    deltaT_YZ_h2->GetZaxis()->SetTitleOffset(0.5);
+    deltaT_YZ_h2->GetZaxis()->SetTitleOffset(0);
     deltaT_YZ_h2->GetZaxis()->SetRangeUser(4575,4620);
     c1->SaveAs("p03_2D_dT_med_full.png");
 
     TCanvas *c2 = new TCanvas("c2", "c2", 1200, 1000);
-    deltaT_YZ_h2_neg->SetMarkerSize(0.8);
+    deltaT_YZ_h2_neg->SetMarkerSize(0.7);
+    deltaT_YZ_h2_neg->SetTitle("beam right (x<0) ;z (cm);y (cm); dT (ticks)");
     deltaT_YZ_h2_neg->Draw("COLZ TEXT");
     deltaT_YZ_h2_neg->GetZaxis()->SetRangeUser(4575,4620);
-    deltaT_YZ_h2_neg->GetZaxis()->SetTitleOffset(0.5);
+    deltaT_YZ_h2_neg->GetZaxis()->SetTitleOffset(0);
     c2->SaveAs("p04_2D_dT_med_full_neg.png");
 
 }
